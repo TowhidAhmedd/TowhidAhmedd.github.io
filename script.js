de8d43d8138771e616d705c3735c3e4407393120
TowhidAhmedd <towhid4635.com>
2026-04-15 10:29:40 +0600

    index

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-<<<<<<< HEAD
+
+
+
+
+
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({
+                behavior: "smooth"
+            });
+    });
+});
+
+
+
+
 
 
 
@@ -24,32 +43,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 
-
-=======
-
-
-
-
-
-
-
-
-// Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href"))
-            .scrollIntoView({
-                behavior: "smooth"
-            });
-    });
-});
-
-
-
-
-
-
-
-
->>>>>>> 9fe99ca5986f7958901144f8ed2e0272de16cd3f
